85e9f1f688b1266b6955a5588897094eb552bab3
Steven Leiva <leiva.steven@gmail.com>
2020-05-14 21:18:21 -0500

    Display all topics.

--- components/TagList.js
+++ components/TagList.js
@@ -1,3 +1,34 @@
+function tagsToSections(tags) {
+  const fn = function (acc, tagName) {
+    const capitalizedFirstChar = tagName[0].toUpperCase();
+
+    const existingTagNames = acc[capitalizedFirstChar] || [];
+
+    const newTagNames = [...existingTagNames, tagName];
+
+    acc[capitalizedFirstChar] = newTagNames;
+
+    return acc;
+  };
+
+  return tags.reduce(fn, {});
+}
+
+function TagSection({ char, tags }) {
+  return (
+    <div className="tag-section">
+      <p>char: {char}</p>
+      <p>tags: {JSON.stringify(tags)}</p>
+    </div>
+  );
+}
+
 export default function TagList({ tags }) {
-  return <p>{JSON.stringify(tags)}</p>;
+  const charToTags = tagsToSections(tags);
+
+  const TagSections = Object.keys(charToTags).map((char) => (
+    <TagSection key={char} char={char} tags={charToTags[char]} />
+  ));
+
+  return <div className="tag-list">{TagSections}</div>;
 }
