32a86eeeba7a18d993f69f4c92f90deed8e1961d
Steven Leiva <leiva.steven@gmail.com>
2020-05-15 13:53:06 -0500

    Add BootStrap's static assets.

--- components/Layout.js
+++ components/Layout.js
@@ -5,14 +5,30 @@ export default function Layout({ children, pageTitle, ...props }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charset="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <link
+          rel="stylesheet"
+          href="/static/css/bootstrap-4.5.0/bootstrap.min.css"
+        />
+
         <title>{pageTitle}</title>
       </Head>
+
       <section className="layout">
         <Header />
+
         <div className="content">{children}</div>
       </section>
       <footer>Built by me!</footer>
+
+      <script src="/static/js/jquery/jquery-3.5.1.slim.min.js"></script>
+      <script src="/static/js/popper-1.16.0/popper.min.js"></script>
+      <script src="/static/js/bootstrap-4.5.0/bootstrap.min.js"></script>
     </>
   );
 }
